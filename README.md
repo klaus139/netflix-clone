f965ce48dddb2331f811dd68295891da390ba244
Nicholas igunbor <nickoklaus5@gmail.com>
2023-01-20 10:57:04 +0100

    netflix clone lets begin

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # netflix-clone
 A movie viewing application made with react and firebase
+#7d97fe3327d192d46dd8922d97d86bf7
---
+++ netflix-clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>lets build netflix clone</h1>
+      
+    </div>
+  );
+}
+
+export default App;

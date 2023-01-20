490c45cd383cc41f69da6c4f531750ad95bf0373
Nicholas igunbor <nickoklaus5@gmail.com>
2023-01-20 13:47:36 +0100

    fecthed all the movies based on categories

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Row from './components/Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>lets build netflix clone</h1>
-      <Row  title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
-      
+      <Row  title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "../axios"; //you can actually change the name of a default import just like i did here.
+import './row.css';
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row() {
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // a snippet of code that makes a request on an api to TMDB
+  useEffect(() => {
+    //if the bracket is blank it means run once when the row loads and dont run it again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
   return (
-    <div>Row</div>
-  )
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+          key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+          
+
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
